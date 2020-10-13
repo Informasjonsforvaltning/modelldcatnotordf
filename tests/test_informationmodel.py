aaffb8a5c7302dffa5df9e80128a12611da26348
Frederik Rønnevig <frederik.code@gmail.com>
2020-10-13 09:58:29 +0200

    feat: #1 Added parser for modellcatnotordf - WIP

--- tests/test_informationmodel.py
+++ tests/test_informationmodel.py
@@ -1,8 +1,10 @@
 """Test cases for the informationmodel module."""
 
 import pytest
+from rdflib import Graph
+from rdflib.compare import isomorphic, graph_diff
 
-from modelldcatnotordf import InformationModel
+from src.modelldcatnotordf.informationmodel import InformationModel
 
 """
 A test class for testing the _abstract_ class Resource.
@@ -16,3 +18,51 @@ def test_instantiate_InformationModel() -> None:
         _ = InformationModel()
     except Exception:
         pytest.fail("Unexpected Exception ..")
+
+
+
+
+def test_to_graph_should_return_title() -> None:
+    """It returns a title graph isomorphic to spec."""
+    """It returns an identifier graph isomorphic to spec."""
+    informationmodel = InformationModel()
+    informationmodel.identifier = "http://example.com/informationmodels/1"
+    informationmodel.title = {"nb": "Tittel 1", "en": "Title 1"}
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://example.com/informationmodels/1> a dcat:Resource ;
+            dct:title   "Title 1"@en, "Tittel 1"@nb ;
+    .
+    """
+    g1 = Graph().parse(data=informationmodel.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+# ---------------------------------------------------------------------- #
+# Utils for displaying debug information
+
+
+def _dump_diff(g1: Graph, g2: Graph) -> None:
+    in_both, in_first, in_second = graph_diff(g1, g2)
+    print("\nin both:")
+    _dump_turtle(in_both)
+    print("\nin first:")
+    _dump_turtle(in_first)
+    print("\nin second:")
+    _dump_turtle(in_second)
+
+
+def _dump_turtle(g: Graph) -> None:
+    for _l in g.serialize(format="turtle").splitlines():
+        if _l:
+            print(_l.decode())
