eed9161aa8071e7d9511c1b00003056129314a9d
Frederik Rønnevig <frederik.code@gmail.com>
2020-10-21 11:50:37 +0200

    feat: #2 Adding publisher field

--- tests/test_informationmodel.py
+++ tests/test_informationmodel.py
@@ -48,7 +48,7 @@ def test_to_graph_should_return_title_and_identifier() -> None:
 
 
 def test_to_graph_should_return_description() -> None:
-    """It returns a title graph isomorphic to spec."""
+    """It returns a description graph isomorphic to spec."""
     """It returns an identifier graph isomorphic to spec."""
     informationmodel = InformationModel()
     informationmodel.identifier = "http://example.com/informationmodels/1"
@@ -75,7 +75,7 @@ def test_to_graph_should_return_description() -> None:
 
 
 def test_to_graph_should_return_theme() -> None:
-    """It returns a title graph isomorphic to spec."""
+    """It returns a theme graph isomorphic to spec."""
     """It returns an identifier graph isomorphic to spec."""
     informationmodel = InformationModel()
     informationmodel.identifier = "http://example.com/informationmodels/1"
@@ -91,6 +91,32 @@ def test_to_graph_should_return_theme() -> None:
     <http://example.com/informationmodels/1> a dcat:Resource ;
                dcat:theme   <http://example.com/themes/1> ,
                      <http://example.com/themes/2> ;
+        .
+    """
+    g1 = Graph().parse(data=informationmodel.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
+def test_to_graph_should_return_publisher() -> None:
+    """It returns a publisher graph isomorphic to spec."""
+    informationmodel = InformationModel()
+    informationmodel.identifier = "http://example.com/informationmodels/1"
+    informationmodel.publisher = "http://example.com/publisher/1"
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+
+    <http://example.com/informationmodels/1> a dcat:Resource ;
+        dct:publisher   <http://example.com/publisher/1> ;
         .
     """
     g1 = Graph().parse(data=informationmodel.to_rdf(), format="turtle")
