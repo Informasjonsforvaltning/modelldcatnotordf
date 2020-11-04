ffb897cbcbb23c5e1dde9689a39bd4d84b42db23
Frederik Rønnevig <frederik.code@gmail.com>
2020-11-04 12:14:06 +0100

    feat: #3 Adding support for dct:identifier on modelldcatno:Property

--- src/modelldcatnotordf/property.py
+++ src/modelldcatnotordf/property.py
@@ -5,9 +5,10 @@ for use in the modelldcat-ap-no specification._
 
 Refer to sub-class for typical usage examples.
 """
+from typing import Optional
 
 from datacatalogtordf import URI
-from rdflib import Graph, Namespace
+from rdflib import Graph, Namespace, RDF, URIRef
 
 MODELLDCATNO = Namespace("https://data.norge.no/vocabulary/modelldcatno#")
 DCT = Namespace("http://purl.org/dc/terms/")
@@ -25,3 +26,37 @@ class Property:
     def __init__(self) -> None:
         """Inits an object with default values."""
         self._type = MODELLDCATNO.Property
+
+    @property
+    def identifier(self) -> str:
+        """Get/set for identifier."""
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier: str) -> None:
+        self._identifier = URI(identifier)
+
+    def to_rdf(self, format: str = "turtle", encoding: Optional[str] = "utf-8") -> str:
+        """Maps the property to rdf.
+
+        Args:
+            format: a valid format. Default: turtle
+            encoding: the encoding to serialize into
+
+        Returns:
+            a rdf serialization as a string according to format.
+        """
+        return self._to_graph().serialize(format=format, encoding=encoding)
+
+    def _to_graph(self) -> Graph:
+        """Returns the property as graph.
+
+        Returns:
+            the property graph
+        """
+        self._g = Graph()
+        self._g.bind("modelldcatno", MODELLDCATNO)
+
+        self._g.add((URIRef(self.identifier), RDF.type, URIRef(self._type)))
+
+        return self._g
