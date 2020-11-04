56021d2313e35b4eefcbf9c69f10c84f266059c3
Frederik Rønnevig <frederik.code@gmail.com>
2020-11-04 12:14:06 +0100

    feat: #3 Adding support for dct:identifier on modelldcatno:Property

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,6 +1,8 @@
 """Test cases for the property module."""
 
 import pytest
+from rdflib import Graph
+from rdflib.compare import graph_diff, isomorphic
 
 from modelldcatnotordf.property import Property
 
@@ -16,3 +18,50 @@ def test_instantiate_modelelement() -> None:
         _ = Property()
     except Exception:
         pytest.fail("Unexpected Exception ..")
+
+
+def test_to_graph_should_return_title_and_identifier() -> None:
+    """It returns a title graph isomorphic to spec."""
+    """It returns an identifier graph isomorphic to spec."""
+
+    property = Property()
+    property.identifier = "http://example.com/propertys/1"
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/propertys/1> a modelldcatno:Property .
+
+        """
+    g1 = Graph().parse(data=property.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
+# ---------------------------------------------------------------------- #
+# Utils for displaying debug information
+
+
+def _dump_diff(g1: Graph, g2: Graph) -> None:
+    in_both, in_first, in_second = graph_diff(g1, g2)
+    print("\nin both:")
+    _dump_turtle(in_both)
+    print("\nin first:")
+    _dump_turtle(in_first)
+    print("\nin second:")
+    _dump_turtle(in_second)
+
+
+def _dump_turtle(g: Graph) -> None:
+    for _l in g.serialize(format="turtle").splitlines():
+        if _l:
+            print(_l.decode())
