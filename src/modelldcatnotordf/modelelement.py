2c9b9cfb7eec473c8ef1c39d57ac8b3e65db2953
Frederik Rønnevig <frederik.code@gmail.com>
2020-11-04 10:15:03 +0100

    feat: #3 Modelelement; identifier, title

--- src/modelldcatnotordf/modelelement.py
+++ src/modelldcatnotordf/modelelement.py
@@ -5,7 +5,10 @@ for use in the modelldcat-ap-no specification._
 
 Refer to sub-class for typical usage examples.
 """
-from rdflib import Namespace
+from typing import Optional
+
+from datacatalogtordf import URI
+from rdflib import Graph, Literal, Namespace, RDF, URIRef
 
 MODELLDCATNO = Namespace("https://data.norge.no/vocabulary/modelldcatno#")
 DCT = Namespace("http://purl.org/dc/terms/")
@@ -15,4 +18,67 @@ DCAT = Namespace("http://www.w3.org/ns/dcat#")
 class ModelElement:
     """A class representing a modelldcatno:ModelElement."""
 
-    pass
+    __slots__ = ("_type", "_g", "_title", "_identifier")
+
+    _g: Graph
+    _title: dict
+    _identifier: URI
+
+    def __init__(self) -> None:
+        """Inits an object with default values."""
+        self._type = MODELLDCATNO.ModelElement
+
+    @property
+    def identifier(self) -> str:
+        """Get/set for identifier."""
+        return self._identifier
+
+    @identifier.setter
+    def identifier(self, identifier: str) -> None:
+        self._identifier = URI(identifier)
+
+    @property
+    def title(self) -> dict:
+        """Title attribute."""
+        return self._title
+
+    @title.setter
+    def title(self, title: dict) -> None:
+        self._title = title
+
+    def to_rdf(self, format: str = "turtle", encoding: Optional[str] = "utf-8") -> str:
+        """Maps the modelelement to rdf.
+
+        Args:
+            format: a valid format. Default: turtle
+            encoding: the encoding to serialize into
+
+        Returns:
+            a rdf serialization as a string according to format.
+        """
+        return self._to_graph().serialize(format=format, encoding=encoding)
+
+    def _to_graph(self) -> Graph:
+        """Returns the modelelement as graph.
+
+        Returns:
+            the modelelement graph
+        """
+        self._g = Graph()
+        self._g.bind("modelldcatno", MODELLDCATNO)
+
+        self._g.add((URIRef(self.identifier), RDF.type, URIRef(self._type)))
+        self._title_to_graph()
+
+        return self._g
+
+    def _title_to_graph(self) -> None:
+        if getattr(self, "title", None):
+            for key in self.title:
+                self._g.add(
+                    (
+                        URIRef(self.identifier),
+                        DCT.title,
+                        Literal(self.title[key], lang=key),
+                    )
+                )
--- tests/test_modelelement.py
+++ tests/test_modelelement.py
@@ -2,7 +2,7 @@
 
 import pytest
 from rdflib import Graph
-from rdflib.compare import graph_diff
+from rdflib.compare import graph_diff, isomorphic
 
 from modelldcatnotordf.modelelement import ModelElement
 
@@ -18,6 +18,35 @@ def test_instantiate_modelelement() -> None:
         _ = ModelElement()
     except Exception:
         pytest.fail("Unexpected Exception ..")
+
+
+def test_to_graph_should_return_title_and_identifier() -> None:
+    """It returns a title graph isomorphic to spec."""
+    """It returns an identifier graph isomorphic to spec."""
+
+    modelelement = ModelElement()
+    modelelement.identifier = "http://example.com/modelelements/1"
+    modelelement.title = {"nb": "Tittel 1", "en": "Title 1"}
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/modelelements/1> a modelldcatno:ModelElement;
+                dct:title   "Title 1"@en, "Tittel 1"@nb ;
+        .
+        """
+    g1 = Graph().parse(data=modelelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
 
 
 # ---------------------------------------------------------------------- #
