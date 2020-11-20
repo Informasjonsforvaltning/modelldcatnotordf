835133cecfa0e692485ccdd357bdd2fbfd3cac0b
Frederik Rønnevig <frederik.code@gmail.com>
2020-11-20 12:47:01 +0100

    feat: #3 dct:subject on modelldcatno:ModelElement

--- tests/test_modelelement.py
+++ tests/test_modelelement.py
@@ -103,6 +103,35 @@ def test_to_graph_should_return_organizationid_as_graph() -> None:
     assert _isomorphic
 
 
+def test_to_graph_should_return_subject() -> None:
+    """It returns a subject graph isomorphic to spec."""
+    modelelement = ModelElement()
+    modelelement.identifier = "http://example.com/modelelements/1"
+    modelelement.subject = "http://example.com/subjects/1"
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+    <http://example.com/modelelements/1> a modelldcatno:ModelElement ;
+        skos:Concept <http://example.com/subjects/1> ;
+
+    .
+    """
+    g1 = Graph().parse(data=modelelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+        pass
+    assert _isomorphic
+
+
 # ---------------------------------------------------------------------- #
 # Utils for displaying debug information
 
