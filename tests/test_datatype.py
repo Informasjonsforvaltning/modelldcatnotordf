a7bc5332f0bbb6316c660de4c10b617608187d5a
Frederik Rønnevig <frederik.code@gmail.com>
2020-12-08 09:58:34 +0100

    feat: #3 modelldcatno:DataType

---
+++ tests/test_datatype.py
@@ -0,0 +1,133 @@
+"""Test cases for the data type module."""
+
+from concepttordf import Concept
+import pytest
+from rdflib import Graph
+from rdflib.compare import isomorphic
+
+from modelldcatnotordf.modelldcatno import DataType
+from tests.testutils import _dump_diff
+
+"""
+A test class for testing the class DataType.
+
+"""
+
+
+def test_instantiate_datatype() -> None:
+    """It returns a TypeErro exception."""
+    try:
+        _ = DataType()
+    except Exception:
+        pytest.fail("Unexpected Exception ..")
+
+
+def test_to_graph_should_return_title_and_identifier() -> None:
+    """It returns a title graph isomorphic to spec."""
+    """It returns an identifier graph isomorphic to spec."""
+
+    datatype = DataType()
+    datatype.identifier = "http://example.com/datatypes/1"
+    datatype.title = {"nb": "Tittel 1", "en": "Title 1"}
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/datatypes/1> a modelldcatno:DataType;
+                dct:title   "Title 1"@en, "Tittel 1"@nb ;
+        .
+        """
+    g1 = Graph().parse(data=datatype.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+    assert _isomorphic
+
+
+def test_to_graph_should_return_title_and_no_identifier() -> None:
+    """It returns a title graph isomorphic to spec."""
+    datatype = DataType()
+    datatype.title = {"nb": "Tittel 1", "en": "Title 1"}
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        [ a modelldcatno:DataType ;
+            dct:title   "Title 1"@en, "Tittel 1"@nb ;
+        ]
+        .
+        """
+    g1 = Graph().parse(data=datatype.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+    assert _isomorphic
+
+
+def test_to_graph_should_return_dct_identifier_as_graph() -> None:
+    """It returns a dct_identifier graph isomorphic to spec."""
+    datatype = DataType()
+    datatype.identifier = "http://example.com/datatypes/1"
+    datatype.dct_identifier = "123456789"
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+    <http://example.com/datatypes/1>    a modelldcatno:DataType ;
+        dct:identifier "123456789";
+    .
+    """
+    g1 = Graph().parse(data=datatype.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+    assert _isomorphic
+
+
+def test_to_graph_should_return_subject() -> None:
+    """It returns a subject graph isomorphic to spec."""
+    datatype = DataType()
+    datatype.identifier = "http://example.com/datatypes/1"
+    subject = Concept()
+    subject.identifier = "https://example.com/subjects/1"
+    datatype.subject = subject
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+    <http://example.com/datatypes/1> a modelldcatno:DataType ;
+        dct:subject <https://example.com/subjects/1> ;
+    .
+     <https://example.com/subjects/1> a skos:Concept .
+
+    """
+    g1 = Graph().parse(data=datatype.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    _isomorphic = isomorphic(g1, g2)
+    if not _isomorphic:
+        _dump_diff(g1, g2)
+    assert _isomorphic
