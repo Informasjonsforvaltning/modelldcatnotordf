bf91f2962cd8178e7a95f385f1d98595d023c8d5
Frederik Rønnevig <frederik.code@gmail.com>
2020-12-11 08:49:15 +0100

    feat: #2 skos:inScheme on modelldcatno:CodeElement

--- tests/test_code_element.py
+++ tests/test_code_element.py
@@ -1,10 +1,11 @@
 """A test class for testing the class CodeElement."""
+from typing import List
 
 from concepttordf import Concept
 import pytest
 from rdflib import Graph
 
-from modelldcatnotordf.modelldcatno import CodeElement
+from modelldcatnotordf.modelldcatno import CodeElement, CodeList
 from tests.testutils import assert_isomorphic
 
 
@@ -151,6 +152,130 @@ def test_to_graph_should_return_notation_as_graph() -> None:
         skos:notation "str";
     .
     """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_in_scheme_both_identifiers() -> None:
+    """It returns a is_codeelement_of graph isomorphic to spec."""
+    codeelement = CodeElement()
+    codeelement.identifier = "http://example.com/codeelements/1"
+
+    codelist = CodeList()
+    codelist.identifier = "http://example.com/codelists/1"
+
+    inschemes: List[CodeList] = [codelist]
+
+    codeelement.in_scheme = inschemes
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+
+        <http://example.com/codeelements/1> a modelldcatno:CodeElement ;
+            skos:inScheme <http://example.com/codelists/1> .
+
+        <http://example.com/codelists/1> a modelldcatno:CodeList ;
+
+        .
+        """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_in_scheme_blank_node_codeelement_identifier() -> None:
+    """It returns a is_codeelement_of graph isomorphic to spec."""
+    codeelement = CodeElement()
+    codeelement.identifier = "http://example.com/codeelements/1"
+
+    codelist = CodeList()
+
+    inschemes: List[CodeList] = [codelist]
+
+    codeelement.in_scheme = inschemes
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+
+        <http://example.com/codeelements/1> a modelldcatno:CodeElement ;
+            skos:inScheme [ a modelldcatno:CodeList ] .
+
+        """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_is_codeelement_of_blank_nodes() -> None:
+    """It returns a is_codeelement_of graph isomorphic to spec."""
+    codeelement = CodeElement()
+
+    codelist = CodeList()
+
+    inschemes: List[CodeList] = [codelist]
+
+    codeelement.in_scheme = inschemes
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+        [ a modelldcatno:CodeElement ;
+            skos:inScheme [ a modelldcatno:CodeList ]
+        ] .
+        """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_in_scheme_bnode_codelist_id() -> None:
+    """It returns a is_codeelement_of graph isomorphic to spec."""
+    codeelement = CodeElement()
+
+    codelist = CodeList()
+    codelist.identifier = "http://example.com/codelists/1"
+
+    inschemes: List[CodeList] = [codelist]
+
+    codeelement.in_scheme = inschemes
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+        [ a modelldcatno:CodeElement ;
+            skos:inScheme <http://example.com/codelists/1>
+        ] .
+
+        <http://example.com/codelists/1> a modelldcatno:CodeList .
+
+        """
     g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
