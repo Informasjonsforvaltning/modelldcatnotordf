f2e9f318019d9001eeb420d52cb4a6c08c655056
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-08 15:22:48 +0100

    feat: #4 skos:hiddenLabel on modelldcatno:CodeElement

--- tests/test_code_element.py
+++ tests/test_code_element.py
@@ -479,3 +479,27 @@ def test_to_graph_should_return_example() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_hiddenlabel() -> None:
+    """It returns a hiddenlabel graph isomorphic to spec."""
+    codeelement = CodeElement()
+    codeelement.identifier = "http://example.com/codeelements/1"
+    codeelement.hiddenlabel = {"nb": "En uegnet betegnelse", "en": "A hidden label"}
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+    <http://example.com/codeelements/1> a modelldcatno:CodeElement ;
+        skos:hiddenLabel "A hidden label"@en, "En uegnet betegnelse"@nb
+    .
+    """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
