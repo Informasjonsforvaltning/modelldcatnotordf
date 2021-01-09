3e6af326fa1c8b2f81d0b55dde5975a5ed4df931
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-09 08:55:07 +0100

    feat: #4 xkos:exclusionNote on modelldcatno:CodeElement

--- tests/test_code_element.py
+++ tests/test_code_element.py
@@ -554,3 +554,33 @@ def test_to_graph_should_return_scopenote() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_exclusion_note() -> None:
+    """It returns a exclusion_note graph isomorphic to spec."""
+    codeelement = CodeElement()
+    codeelement.identifier = "http://example.com/codeelements/1"
+    codeelement.exclusion_note = {
+        "en": "An exclusion note",
+        "nb": "En merknad ang. ekskluderte elementer",
+    }
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+    @prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> .
+
+
+    <http://example.com/codeelements/1> a modelldcatno:CodeElement ;
+        xkos:exclusionNote "An exclusion note"@en,
+                            "En merknad ang. ekskluderte elementer"@nb
+    .
+    """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
