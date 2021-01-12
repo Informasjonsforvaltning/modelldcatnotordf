4fe3897b5cad52bbac5bbd20f18521125ba88d41
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-12 13:47:36 +0100

    feat: #4 foaf:homepage on modelldcatno:InformationModel

--- tests/test_informationmodel.py
+++ tests/test_informationmodel.py
@@ -598,3 +598,27 @@ def test_to_graph_should_return_language() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_homepage() -> None:
+    """It returns a homepage graph isomorphic to spec."""
+    infomationmodel = InformationModel()
+    infomationmodel.identifier = "http://example.com/informationmodels/1"
+    infomationmodel.homepage = "http://example.org/informationmodel"
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix foaf:  <http://xmlns.com/foaf/0.1/> .
+
+    <http://example.com/informationmodels/1> a modelldcatno:InformationModel ;
+        foaf:homepage <http://example.org/informationmodel> ;
+    .
+    """
+    g1 = Graph().parse(data=infomationmodel.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
