9e5fbadaead114984a7f52051765b653251a17c5
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-18 12:19:50 +0100

    feat: #4 dct:creator on modelldcatno:InformationModel

--- tests/test_informationmodel.py
+++ tests/test_informationmodel.py
@@ -896,3 +896,30 @@ def test_to_graph_should_return_status() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_creator() -> None:
+    """It returns a creator graph isomorphic to spec."""
+    informationmodel = InformationModel()
+    informationmodel.identifier = "http://example.com/informationmodels/1"
+    informationmodel.creator = "http://example.com/creator/1"
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix foaf:  <http://xmlns.com/foaf/0.1/> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+        @prefix owl: <http://www.w3.org/2002/07/owl#> .
+        @prefix adms: <http://www.w3.org/ns/adms#> .
+
+        <http://example.com/informationmodels/1> a modelldcatno:InformationModel ;
+             dct:creator   <http://example.com/creator/1> ;
+        .
+    """
+    g1 = Graph().parse(data=informationmodel.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
