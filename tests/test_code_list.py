ea82031e1cf7927f1c6ee2e98cb56b73abbc3d7b
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-18 13:47:24 +0100

    feat: #4 modelldcatno:codeListReferencer on modelldcatno:CodeList

--- tests/test_code_list.py
+++ tests/test_code_list.py
@@ -118,3 +118,65 @@ def test_to_graph_should_return_subject() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_code_list_reference() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    codelist1 = CodeList()
+    codelist1.identifier = "http://example.com/codelists/1"
+
+    codelist2 = CodeList()
+    codelist2.identifier = "http://example.com/codelists/2"
+
+    codelist1.code_list_reference = codelist2
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+        @prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> .
+
+
+        <http://example.com/codelists/1>
+                a modelldcatno:CodeList;
+                    modelldcatno:codeListReference <http://example.com/codelists/2> .
+
+        <http://example.com/codelists/2> a modelldcatno:CodeList .
+
+        """
+    g1 = Graph().parse(data=codelist1.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_code_list_reference_bnode() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    codelist1 = CodeList()
+    codelist1.identifier = "http://example.com/codelists/1"
+
+    codelist2 = CodeList()
+
+    codelist1.code_list_reference = codelist2
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+        @prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> .
+
+        <http://example.com/codelists/1>
+                a modelldcatno:CodeList;
+                    modelldcatno:codeListReference [ a modelldcatno:CodeList ]  .
+
+        """
+    g1 = Graph().parse(data=codelist1.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
