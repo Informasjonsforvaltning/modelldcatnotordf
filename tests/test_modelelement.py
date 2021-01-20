1bde77134762847abd79c5c76ddc58f62f248b9e
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-20 14:28:15 +0100

    feat: #4 modelldcatno:belongsToModule on modelldcatno:ModelElement

--- tests/test_modelelement.py
+++ tests/test_modelelement.py
@@ -226,3 +226,26 @@ def test_to_graph_should_return_has_property_blank_nodes() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_belongs_to_module_as_graph() -> None:
+    """It returns a belongs_to_module graph isomorphic to spec."""
+    modelelement = ObjectType()
+    modelelement.identifier = "http://example.com/modelelements/1"
+    modelelement.belongs_to_module = ["core"]
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+    <http://example.com/modelelements/1>    a modelldcatno:ObjectType ;
+        modelldcatno:belongsToModule "core";
+    .
+    """
+    g1 = Graph().parse(data=modelelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
