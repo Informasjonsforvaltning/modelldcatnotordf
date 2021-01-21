9eb2a733b6cb11eafec1fdbecb4f69567758ef39
Frederik Rønnevig <frederik.code@gmail.com>
2021-01-21 10:40:11 +0100

    feat: #4 dct:description on modelldcatno:ModelProperty

--- tests/test_property.py
+++ tests/test_property.py
@@ -268,3 +268,28 @@ def test_to_graph_should_return_subject() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_description() -> None:
+    """It returns a description graph isomorphic to spec."""
+    """It returns an identifier graph isomorphic to spec."""
+    modelproperty = Role()
+    modelproperty.identifier = "http://example.com/modelpropertys/1"
+    modelproperty.description = {"nb": "Beskrivelse", "en": "Description"}
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+
+    <http://example.com/modelpropertys/1> a modelldcatno:Role ;
+            dct:description   "Description"@en, "Beskrivelse"@nb ;
+    .
+    """
+    g1 = Graph().parse(data=modelproperty.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
