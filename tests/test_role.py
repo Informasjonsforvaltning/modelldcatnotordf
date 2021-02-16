aa38fcd76b54a08f19674bec495fd02a2b631179
Frederik Rønnevig <frederik.code@gmail.com>
2021-02-16 15:29:48 +0100

    fix: #63 Correcting serialization on modelldcatno:hasObjectType on modelldcatno:Role

--- tests/test_role.py
+++ tests/test_role.py
@@ -267,3 +267,28 @@ def test_to_graph_should_return_subject() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_has_object_type_both_identifiers_as_uri() -> None:
+    """It returns a has_object_type graph isomorphic to spec."""
+    role = Role()
+    role.identifier = "http://example.com/roles/1"
+
+    objecttype = "http://example.com/objecttypes/1"
+    role.has_object_type = objecttype
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/roles/1> a modelldcatno:Role ;
+            modelldcatno:hasObjectType <http://example.com/objecttypes/1> .
+
+        """
+    g1 = Graph().parse(data=role.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
