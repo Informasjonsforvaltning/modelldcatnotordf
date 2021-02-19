4c9b00ddc3f3829688a956ecad3fdabedbd7db1b
Frederik Rønnevig <frederik.code@gmail.com>
2021-02-19 14:12:31 +0100

    fix: #63 Correcting serialization on skos:Concept on modelldcatno:CodeElement

--- tests/test_code_element.py
+++ tests/test_code_element.py
@@ -740,3 +740,29 @@ def test_to_graph_should_return_previous_bnode() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_subject_as_uri() -> None:
+    """It returns a subject graph isomorphic to spec."""
+    codeelement = CodeElement()
+    codeelement.identifier = "http://example.com/codeelements/1"
+    subject = "https://example.com/subjects/1"
+    codeelement.subject = subject
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+
+    <http://example.com/codeelements/1> a modelldcatno:CodeElement ;
+        dct:subject <https://example.com/subjects/1> ;
+    .
+
+    """
+    g1 = Graph().parse(data=codeelement.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
