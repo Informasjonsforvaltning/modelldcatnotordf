372eaa601566a84876f718ceba24569326348141
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-08 14:56:59 +0100

    feat: #73 Refactoring skolemization logic

--- tests/test_skolemizer.py
+++ tests/test_skolemizer.py
@@ -1,7 +1,6 @@
 """Test cases for the skolemizer module."""
 import os
 
-from modelldcatnotordf.modelldcatno import ObjectType
 from modelldcatnotordf.modelldcatno import Skolemizer
 
 """
@@ -12,76 +11,20 @@ A test class for testing the class InformationModel.
 
 def test_add_skolemization() -> None:
     """Tests skolemization."""
-    os.environ[
-        Skolemizer.baseurl_key
-    ] = "https://altinn-model-publisher.digdir.no/models/4985-5704/"
+    skolemization1 = Skolemizer.add_skolemization()
+    skolemization2 = Skolemizer.add_skolemization()
 
-    modelelement = ObjectType()
-
-    _classtype = modelelement.__class__.__name__
-
-    skolemization1 = (
-        "https://altinn-model-publisher.digdir.no/models/4985-5704/ObjectType/1"
+    assert skolemization1.startswith(
+        Skolemizer.baseurl_default_value + ".well-known/skolem/"
     )
-
-    skolemization2 = (
-        "https://altinn-model-publisher.digdir.no/models/4985-5704/ObjectType/2"
-    )
-
-    assert not Skolemizer.is_exact_skolemization(skolemization1)
-    assert skolemization1 == Skolemizer.add_skolemization(_classtype)
+    assert Skolemizer.has_skolemization_morfologi(skolemization1)
     assert Skolemizer.is_exact_skolemization(skolemization1)
-    assert not Skolemizer.is_exact_skolemization(skolemization2)
-    assert skolemization2 == Skolemizer.add_skolemization(_classtype)
+
+    assert skolemization2.startswith(
+        Skolemizer.baseurl_default_value + ".well-known/skolem/"
+    )
+    assert Skolemizer.has_skolemization_morfologi(skolemization2)
     assert Skolemizer.is_exact_skolemization(skolemization2)
-
-
-def test_has_skolemization_morfologi_success() -> None:
-    """Tests skolemization morfologi."""
-    os.environ[
-        Skolemizer.baseurl_key
-    ] = "https://altinn-model-publisher.digdir.no/models/4985-5704/"
-
-    _skolemization = (
-        "https://altinn-model-publisher.digdir.no/models/4985-5704/ObjectType/2"
-    )
-
-    assert Skolemizer.has_skolemization_morfologi(_skolemization)
-
-
-def test_has_skolemization_morfologi_incorrect_identifier() -> None:
-    """Tests skolemization morfologi."""
-    os.environ[
-        Skolemizer.baseurl_key
-    ] = "https://altinn-model-publisher.digdir.no/models/4985-5704/"
-
-    _skolemization = "https://someWrongIdentifier/ObjectType/2"
-
-    assert not Skolemizer.has_skolemization_morfologi(_skolemization)
-
-
-def test_has_skolemization_morfologi_incorrect_counter() -> None:
-    """Tests skolemization morfologi."""
-    os.environ[
-        Skolemizer.baseurl_key
-    ] = "https://altinn-model-publisher.digdir.no/models/4985-5704/"
-
-    _skolemization = (
-        "https://altinn-model-publisher.digdir.no/models/4985-5704/ObjectType/X"
-    )
-
-    assert not Skolemizer.has_skolemization_morfologi(_skolemization)
-
-
-def test_has_skolemization_morfologi_incorrect_class() -> None:
-    """Tests skolemization morfologi."""
-    os.environ[
-        Skolemizer.baseurl_key
-    ] = "https://altinn-model-publisher.digdir.no/models/4985-5704/"
-
-    _skolemization = "https://altinn-model-publisher.digdir.no/models/4985-5704/Foo/1"
-
-    assert not Skolemizer.has_skolemization_morfologi(_skolemization)
 
 
 def test_get_baseurl() -> None:
@@ -115,3 +58,15 @@ def test_get_baseurl_missing_slash_at_end() -> None:
     os.environ[Skolemizer.baseurl_key] = Skolemizer.baseurl_default_value[:-1]
 
     assert Skolemizer.get_baseurl() == Skolemizer.baseurl_default_value
+
+
+def test_has_skolemization_morfologi_invalid_url() -> None:
+    """Tests skolemization for invalid url."""
+    skolemization = Skolemizer.add_skolemization()
+
+    assert skolemization.startswith(
+        Skolemizer.baseurl_default_value + ".well-known/skolem/"
+    )
+
+    assert Skolemizer.has_skolemization_morfologi(skolemization)
+    assert not Skolemizer.has_skolemization_morfologi(skolemization + "<")
