da512789c664a886043220407c25d5981a5b8a0b
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-10 15:21:00 +0100

    feat: #86 Adding skolemization to modelldcatno:DataType

--- src/modelldcatnotordf/modelldcatno.py
+++ src/modelldcatnotordf/modelldcatno.py
@@ -2482,9 +2482,9 @@ class DataType(ModelElement):
         Returns:
             the object type graph
         """
-        _self = (
-            URIRef(self.identifier) if getattr(self, "identifier", None) else BNode()
-        )
+        if not getattr(self, "identifier", None):
+            self.identifier = Skolemizer.add_skolemization()
+        _self = URIRef(self.identifier)
 
         super(DataType, self)._to_graph(MODELLDCATNO.DataType, _self)
 
