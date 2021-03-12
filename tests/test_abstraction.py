f65e5f9a4155eedcdff43efff0c45d71a0f73c94
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-12 13:00:08 +0100

    feat: #86 Adding skolemization for modelldcatno:Abstraction

--- tests/test_abstraction.py
+++ tests/test_abstraction.py
@@ -1,10 +1,12 @@
 """Test cases for the abstraction module."""
 
 import pytest
+from pytest_mock import MockFixture
 from rdflib import Graph
 
 from modelldcatnotordf.modelldcatno import Abstraction, ObjectType
-from tests.testutils import assert_isomorphic
+from tests import testutils
+from tests.testutils import assert_isomorphic, skolemization
 
 """
 A test class for testing the class Abstraction.
@@ -20,9 +22,14 @@ def test_instantiate_abstraction() -> None:
         pytest.fail("Unexpected Exception ..")
 
 
-def test_to_graph_should_return_blank_node() -> None:
+def test_to_graph_should_return_blank_skolemization(mocker: MockFixture) -> None:
     """It returns a abstraction graph as blank node isomorphic to spec."""
     abstraction = Abstraction()
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
 
     src = """
         @prefix dct: <http://purl.org/dc/terms/> .
@@ -31,7 +38,8 @@ def test_to_graph_should_return_blank_node() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Abstraction ] .
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Abstraction  .
 
         """
     g1 = Graph().parse(data=abstraction.to_rdf(), format="turtle")
@@ -115,13 +123,20 @@ def test_to_graph_should_return_is_abstr_blank_node_abstr_identifier() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_is_abstr_of_bnode_modelelement_id() -> None:
+def test_to_graph_should_return_is_abstr_of_skolemization_modelelement_id(
+    mocker: MockFixture,
+) -> None:
     """It returns a is_abstraction_of graph isomorphic to spec."""
     abstraction = Abstraction()
 
     modelelement = ObjectType()
     modelelement.identifier = "http://example.com/modelelements/1"
     abstraction.is_abstraction_of = modelelement
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
 
     src = """
         @prefix dct: <http://purl.org/dc/terms/> .
@@ -130,9 +145,10 @@ def test_to_graph_should_return_is_abstr_of_bnode_modelelement_id() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Abstraction ;
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Abstraction ;
             modelldcatno:isAbstractionOf <http://example.com/modelelements/1>
-        ] .
+         .
 
         <http://example.com/modelelements/1> a modelldcatno:ObjectType .
 
@@ -143,12 +159,21 @@ def test_to_graph_should_return_is_abstr_of_bnode_modelelement_id() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_is_abstraction_of_blank_nodes() -> None:
+def test_to_graph_should_return_is_abstraction_of_both_skolemized(
+    mocker: MockFixture,
+) -> None:
     """It returns a is_abstraction_of graph isomorphic to spec."""
     abstraction = Abstraction()
 
     modelelement = ObjectType()
     abstraction.is_abstraction_of = modelelement
+
+    skolemutils = testutils.SkolemUtils()
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        side_effect=skolemutils.get_skolemization,
+    )
 
     src = """
         @prefix dct: <http://purl.org/dc/terms/> .
@@ -157,9 +182,10 @@ def test_to_graph_should_return_is_abstraction_of_blank_nodes() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Abstraction ;
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Abstraction ;
             modelldcatno:isAbstractionOf [ a modelldcatno:ObjectType ]
-        ] .
+         .
         """
     g1 = Graph().parse(data=abstraction.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
