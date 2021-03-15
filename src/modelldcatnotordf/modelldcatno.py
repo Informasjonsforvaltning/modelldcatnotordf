91f0695eb39825bb2c36b888ecde50a684926ebf
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-15 13:11:42 +0100

    feat: #86 Adding skolemization for modelldcatno:CodeList

--- src/modelldcatnotordf/modelldcatno.py
+++ src/modelldcatnotordf/modelldcatno.py
@@ -2596,9 +2596,10 @@ class CodeList(ModelElement):
         Returns:
             the root object type graph
         """
-        _self = (
-            URIRef(self.identifier) if getattr(self, "identifier", None) else BNode()
-        )
+        if not getattr(self, "identifier", None):
+            self.identifier = Skolemizer.add_skolemization()
+
+        _self = URIRef(self.identifier)
 
         super(CodeList, self)._to_graph(MODELLDCATNO.CodeList, _self)
 
