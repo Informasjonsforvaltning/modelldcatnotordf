c6796fe3ab09022e71c4f6442e9e9c1ea5d7a9cf
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-16 14:51:38 +0100

    feat: #86 Adding skolemization for modelldcatno:SimpleType

--- src/modelldcatnotordf/modelldcatno.py
+++ src/modelldcatnotordf/modelldcatno.py
@@ -1583,9 +1583,10 @@ class SimpleType(ModelElement):
         Returns:
             the object type graph
         """
-        _self = (
-            URIRef(self.identifier) if getattr(self, "identifier", None) else BNode()
-        )
+        if not getattr(self, "identifier", None):
+            self.identifier = Skolemizer.add_skolemization()
+
+        _self = URIRef(self.identifier)
 
         super(SimpleType, self)._to_graph(MODELLDCATNO.SimpleType, _self)
 
