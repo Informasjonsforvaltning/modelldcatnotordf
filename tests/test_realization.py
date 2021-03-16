2477fea4fadab99f52e6311594f08dc0c235098e
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-16 14:34:42 +0100

    feat: #86 Adding skolemization for modelldcatno:Realization

--- tests/test_realization.py
+++ tests/test_realization.py
@@ -1,10 +1,12 @@
 """Test cases for the realization module."""
 
 import pytest
+from pytest_mock import MockFixture
 from rdflib import Graph
 
 from modelldcatnotordf.modelldcatno import ObjectType, Realization
-from tests.testutils import assert_isomorphic
+from tests import testutils
+from tests.testutils import assert_isomorphic, skolemization
 
 """
 A test class for testing the class Realization.
@@ -20,7 +22,7 @@ def test_instantiate_realization() -> None:
         pytest.fail("Unexpected Exception ..")
 
 
-def test_to_graph_should_return_blank_node() -> None:
+def test_to_graph_should_return_skolemization(mocker: MockFixture) -> None:
     """It returns a realization graph as blank node isomorphic to spec."""
     realization = Realization()
 
@@ -31,9 +33,14 @@ def test_to_graph_should_return_blank_node() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Realization ] .
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Realization  .
 
         """
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
     g1 = Graph().parse(data=realization.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
@@ -115,7 +122,9 @@ def test_to_graph_should_return_has_supplier_bnode_realization_id() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_has_supplier_bnode_modelelement_id() -> None:
+def test_to_graph_should_return_has_supplier_skolemization_modelelement_id(
+    mocker: MockFixture,
+) -> None:
     """It returns a has_supplier graph isomorphic to spec."""
     realization = Realization()
 
@@ -130,20 +139,29 @@ def test_to_graph_should_return_has_supplier_bnode_modelelement_id() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Realization ;
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Realization ;
             modelldcatno:hasSupplier <http://example.com/modelelements/1>
-        ] .
+         .
 
         <http://example.com/modelelements/1> a modelldcatno:ObjectType .
 
         """
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
     g1 = Graph().parse(data=realization.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_has_supplier_blank_nodes() -> None:
+def test_to_graph_should_return_has_supplier_both_skolemized(
+    mocker: MockFixture,
+) -> None:
     """It returns a has_supplier graph isomorphic to spec."""
     realization = Realization()
 
@@ -157,10 +175,19 @@ def test_to_graph_should_return_has_supplier_blank_nodes() -> None:
         @prefix dcat: <http://www.w3.org/ns/dcat#> .
         @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        [ a modelldcatno:Realization ;
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Realization ;
             modelldcatno:hasSupplier [ a modelldcatno:ObjectType ]
-        ] .
+         .
         """
+
+    skolemutils = testutils.SkolemUtils()
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        side_effect=skolemutils.get_skolemization,
+    )
+
     g1 = Graph().parse(data=realization.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
