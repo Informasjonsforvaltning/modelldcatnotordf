f2268545ff0ef3d61abee39aeff69464b226beac
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-16 14:22:46 +0100

    feat: #86 Adding skolemization for modelldcatno:Note

--- src/modelldcatnotordf/modelldcatno.py
+++ src/modelldcatnotordf/modelldcatno.py
@@ -3190,9 +3190,10 @@ class Note(ModelProperty):
         Returns:
             the role graph
         """
-        _self = (
-            URIRef(self.identifier) if getattr(self, "identifier", None) else BNode()
-        )
+        if not getattr(self, "identifier", None):
+            self.identifier = Skolemizer.add_skolemization()
+
+        _self = URIRef(self.identifier)
 
         super(Note, self)._to_graph(MODELLDCATNO.Note, _self)
 
