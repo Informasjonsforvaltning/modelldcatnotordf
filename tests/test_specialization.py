c131a09b313dfc25497e1e3a6ad7dcc4c099b4b2
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-20 13:20:35 +0100

    feat: #86 Adding skolemization for modelldcatno:hasGeneralConcept

--- tests/test_specialization.py
+++ tests/test_specialization.py
@@ -99,7 +99,9 @@ def test_to_graph_should_return_has_general_concept_both_identifiers() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_has_general_concept_bnode_specialization_id() -> None:
+def test_to_graph_should_return_has_general_concept_skolemization_specialization_id(
+    mocker: MockFixture,
+) -> None:
     """It returns a has_general_concept graph isomorphic to spec."""
     specialization = Specialization()
     specialization.identifier = "http://example.com/specializations/1"
@@ -108,16 +110,26 @@ def test_to_graph_should_return_has_general_concept_bnode_specialization_id() ->
     specialization.has_general_concept = modelelement
 
     src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        <http://example.com/specializations/1> a modelldcatno:Specialization ;
-            modelldcatno:hasGeneralConcept [ a modelldcatno:ObjectType ] .
+    <http://example.com/specializations/1> a modelldcatno:Specialization ;
+        modelldcatno:hasGeneralConcept
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:ObjectType .
 
-        """
+    """
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
     g1 = Graph().parse(data=specialization.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
@@ -171,17 +183,20 @@ def test_to_graph_should_return_has_general_concept_both_skolemized(
     specialization.has_general_concept = modelelement
 
     src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
-         a modelldcatno:Specialization ;
-            modelldcatno:hasGeneralConcept [ a modelldcatno:ObjectType ]
-         .
-        """
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:Specialization ; modelldcatno:hasGeneralConcept
+        <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+        a modelldcatno:ObjectType
+    .
+    """
 
     skolemutils = testutils.SkolemUtils()
 
