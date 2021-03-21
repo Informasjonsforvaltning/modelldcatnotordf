2fcfaf446c2d043f3cf375d909ff2182ba0ab83d
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-21 12:52:57 +0100

    feat: #86 Adding skolemization for modelldcatno:hasSupplier

--- tests/test_realization.py
+++ tests/test_realization.py
@@ -97,7 +97,9 @@ def test_to_graph_should_return_has_supplier_both_identifiers() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_has_supplier_bnode_realization_id() -> None:
+def test_to_graph_should_return_has_supplier_skolemization_realization_id(
+    mocker: MockFixture,
+) -> None:
     """It returns a has_supplier graph isomorphic to spec."""
     realization = Realization()
     realization.identifier = "http://example.com/realizations/1"
@@ -106,16 +108,26 @@ def test_to_graph_should_return_has_supplier_bnode_realization_id() -> None:
     realization.has_supplier = modelelement
 
     src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        <http://example.com/realizations/1> a modelldcatno:Realization ;
-            modelldcatno:hasSupplier [ a modelldcatno:ObjectType ] .
+    <http://example.com/realizations/1> a modelldcatno:Realization ;
+        modelldcatno:hasSupplier
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:ObjectType .
 
-        """
+    """
+
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
     g1 = Graph().parse(data=realization.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
@@ -169,17 +181,21 @@ def test_to_graph_should_return_has_supplier_both_skolemized(
     realization.has_supplier = modelelement
 
     src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
-         a modelldcatno:Realization ;
-            modelldcatno:hasSupplier [ a modelldcatno:ObjectType ]
-         .
-        """
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:Realization ;
+        modelldcatno:hasSupplier
+        <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+        a modelldcatno:ObjectType
+    .
+    """
 
     skolemutils = testutils.SkolemUtils()
 
