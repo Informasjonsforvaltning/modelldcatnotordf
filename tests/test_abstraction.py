880553b5a7e159df100dcfb62c5ffb916798b407
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-21 14:04:13 +0100

    feat: #86 Adding skolemization for modelldcatno:isAbstractionOf

--- tests/test_abstraction.py
+++ tests/test_abstraction.py
@@ -98,7 +98,9 @@ def test_to_graph_should_return_is_abstraction_of_both_identifiers() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_is_abstr_blank_node_abstr_identifier() -> None:
+def test_to_graph_should_return_is_abstr_skolemization_abstr_identifier(
+    mocker: MockFixture,
+) -> None:
     """It returns a is_abstraction_of graph isomorphic to spec."""
     abstraction = Abstraction()
     abstraction.identifier = "http://example.com/abstractions/1"
@@ -107,16 +109,25 @@ def test_to_graph_should_return_is_abstr_blank_node_abstr_identifier() -> None:
     abstraction.is_abstraction_of = modelelement
 
     src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
 
-        <http://example.com/abstractions/1> a modelldcatno:Abstraction ;
-            modelldcatno:isAbstractionOf [ a modelldcatno:ObjectType ] .
+    <http://example.com/abstractions/1> a modelldcatno:Abstraction ;
+        modelldcatno:isAbstractionOf
+        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:ObjectType .
+    """
 
-        """
+    mocker.patch(
+        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
+        return_value=skolemization,
+    )
+
     g1 = Graph().parse(data=abstraction.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
@@ -168,6 +179,23 @@ def test_to_graph_should_return_is_abstraction_of_both_skolemized(
     modelelement = ObjectType()
     abstraction.is_abstraction_of = modelelement
 
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+        a modelldcatno:Abstraction ;
+        modelldcatno:isAbstractionOf
+        <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+    .
+    <http://wwww.digdir.no/.well-known/skolem/21043186-80ce-11eb-9829-cf7c8fc855ce>
+        a modelldcatno:ObjectType
+    .
+    """
+
     skolemutils = testutils.SkolemUtils()
 
     mocker.patch(
@@ -175,18 +203,6 @@ def test_to_graph_should_return_is_abstraction_of_both_skolemized(
         side_effect=skolemutils.get_skolemization,
     )
 
-    src = """
-        @prefix dct: <http://purl.org/dc/terms/> .
-        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-        @prefix dcat: <http://www.w3.org/ns/dcat#> .
-        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
-
-        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
-         a modelldcatno:Abstraction ;
-            modelldcatno:isAbstractionOf [ a modelldcatno:ObjectType ]
-         .
-        """
     g1 = Graph().parse(data=abstraction.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
