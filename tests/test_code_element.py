5b4758af1314c7d96eca1df0d3ed272074f2640a
Frederik Rønnevig <frederik.code@gmail.com>
2021-03-24 09:18:05 +0100

    Revert "feat: #86 Adding skolemization for xkos:next"
    This reverts commit ee5e73faa3e4f6dfc488e22a282aeafbbabcc3e8.

--- tests/test_code_element.py
+++ tests/test_code_element.py
@@ -724,7 +724,7 @@ def test_to_graph_should_return_next() -> None:
     assert_isomorphic(g1, g2)
 
 
-def test_to_graph_should_return_next_skolemization(mocker: MockFixture) -> None:
+def test_to_graph_should_return_next_bnode() -> None:
     """It returns an identifier graph isomorphic to spec."""
     codeelement1 = CodeElement()
     codeelement1.identifier = "http://example.com/codeelements/1"
@@ -734,27 +734,20 @@ def test_to_graph_should_return_next_skolemization(mocker: MockFixture) -> None:
     codeelement1.next_element = codeelement2
 
     src = """
-    @prefix dct: <http://purl.org/dc/terms/> .
-    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-    @prefix dcat: <http://www.w3.org/ns/dcat#> .
-    @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
-    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
-    @prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> .
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+        @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+        @prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> .
 
-    <http://example.com/codeelements/1>
-        a modelldcatno:CodeElement; xkos:next
-        <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
-    .
-    <http://wwww.digdir.no/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
-        a modelldcatno:CodeElement .
-    """
 
-    mocker.patch(
-        "modelldcatnotordf.skolemizer.Skolemizer.add_skolemization",
-        return_value=skolemization,
-    )
+        <http://example.com/codeelements/1>
+                a modelldcatno:CodeElement;
+                    xkos:next [ a modelldcatno:CodeElement ]  .
 
+        """
     g1 = Graph().parse(data=codeelement1.to_rdf(), format="turtle")
     g2 = Graph().parse(data=src, format="turtle")
 
