89f298253f793b76d15c73b8850d29f7ae826856
Frederik Rønnevig <frederik.code@gmail.com>
2021-06-04 09:27:09 +0200

    feat: #113 adding optional identifier to constructor on modelldcatno:ObjectType

--- tests/test_objecttype.py
+++ tests/test_objecttype.py
@@ -47,6 +47,26 @@ def test_to_graph_should_return_title_and_identifier() -> None:
     assert_isomorphic(g1, g2)
 
 
+def test_to_graph_should_return_identifier_set_at_constructor() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    objectype = ObjectType("http://example.com/objectypes/1")
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/objectypes/1> a modelldcatno:ObjectType
+        .
+        """
+    g1 = Graph().parse(data=objectype.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
 def test_to_graph_should_return_title_and_skolemization(mocker: MockFixture) -> None:
     """It returns a title graph isomorphic to spec."""
     objectype = ObjectType()
