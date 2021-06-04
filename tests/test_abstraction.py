73a153a8c9a0e2912123f78da6ff4cdc9906149e
Frederik Rønnevig <frederik.code@gmail.com>
2021-06-04 14:56:59 +0200

    feat: #113 adding optional identifier to constructor on modelldcatno:Abstraction

--- tests/test_abstraction.py
+++ tests/test_abstraction.py
@@ -22,6 +22,26 @@ def test_instantiate_abstraction() -> None:
         pytest.fail("Unexpected Exception ..")
 
 
+def test_to_graph_should_return_identifier_set_at_constructor() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    abstraction = Abstraction("http://example.com/abstractions/1")
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/abstractions/1> a modelldcatno:Abstraction .
+
+        """
+    g1 = Graph().parse(data=abstraction.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
 def test_to_graph_should_return_blank_skolemization(mocker: MockFixture) -> None:
     """It returns a abstraction graph as blank node isomorphic to spec."""
     abstraction = Abstraction()
