7e2e14dadcb27e7e3fcab70586eb8002fc76ccf9
Frederik Rønnevig <frederik.code@gmail.com>
2021-06-07 12:26:49 +0200

    feat: #113 adding optional identifier to constructor on modelldcatno:RootObjectType

--- src/modelldcatnotordf/modelldcatno.py
+++ src/modelldcatnotordf/modelldcatno.py
@@ -2437,8 +2437,10 @@ class RootObjectType(ModelElement):
     _g: Graph
     _belongs_to_module: List[str]
 
-    def __init__(self) -> None:
+    def __init__(self, identifier: Optional[str] = None) -> None:
         """Inits an object with default values."""
+        if identifier:
+            self.identifier = identifier
         super().__init__()
 
     def to_rdf(
