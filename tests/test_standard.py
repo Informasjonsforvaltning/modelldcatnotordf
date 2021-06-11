59c54e9ccf028d184ecf4124a2be99e4bfa211b5
Frederik Rønnevig <frederik.code@gmail.com>
2021-06-11 09:05:18 +0200

    feat: #170 rdfs:seeAlso on dct:Standard

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -94,3 +94,27 @@ def test_to_graph_should_return_title() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_format_and_see_also() -> None:
+    """It returns a rdfs:seeAlso graph isomorphic to spec."""
+    standard = Standard()
+    standard.identifier = "http://example.com/standards/1"
+    standard.has_reference = "http://example.com/link"
+
+    src = """
+    @prefix dct: <http://purl.org/dc/terms/> .
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix dcat: <http://www.w3.org/ns/dcat#> .
+    @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+
+    <http://example.com/standards/1> a dct:Standard;
+        rdfs:seeAlso <http://example.com/link>
+    .
+    """
+
+    g1 = Graph().parse(data=standard.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
