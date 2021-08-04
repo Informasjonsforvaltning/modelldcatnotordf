2910305b0185012c617aad332e30bee04da8da95
Frederik Rønnevig <frederik.code@gmail.com>
2021-08-04 14:45:48 +0200

    feat: #162 New class modelldcatno:Module

---
+++ tests/test_module.py
@@ -0,0 +1,90 @@
+"""Test cases for the modelldcatno:Module module."""
+
+import pytest
+from pytest_mock import MockFixture
+from rdflib import Graph
+from skolemizer.testutils import skolemization
+
+from modelldcatnotordf.modelldcatno import Module
+from tests.testutils import assert_isomorphic
+
+"""
+A test class for testing the class Module.
+
+"""
+
+
+def test_instantiate_module() -> None:
+    """It does not raise an exception."""
+    try:
+        _ = Module()
+    except Exception:
+        pytest.fail("Unexpected Exception ..")
+
+
+def test_to_graph_should_return_identifier_set_at_constructor() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    module = Module("http://example.com/modules/1")
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/modules/1> a modelldcatno:Module .
+
+        """
+    g1 = Graph().parse(data=module.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_skolemization(mocker: MockFixture) -> None:
+    """It returns a modelldcatno:Module graph with skolemization isomorphic to spec."""
+    module = Module()
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+         a modelldcatno:Module .
+
+        """
+
+    mocker.patch(
+        "skolemizer.Skolemizer.add_skolemization", return_value=skolemization,
+    )
+
+    g1 = Graph().parse(data=module.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
+
+
+def test_to_graph_should_return_identifier() -> None:
+    """It returns an identifier graph isomorphic to spec."""
+    module = Module()
+    module.identifier = "http://example.com/modules/1"
+
+    src = """
+        @prefix dct: <http://purl.org/dc/terms/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix dcat: <http://www.w3.org/ns/dcat#> .
+        @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+
+        <http://example.com/modules/1> a modelldcatno:Module .
+
+        """
+
+    g1 = Graph().parse(data=module.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
