07ddbe5c976d32ea5e450f52b06474a35955ebd2
Frederik Rønnevig <frederik.code@gmail.com>
2021-10-20 12:17:00 +0200

    feat: #200 0 integer value serialization for xsd:minOccurs on modellcatno:Property

--- tests/test_property.py
+++ tests/test_property.py
@@ -676,3 +676,35 @@ def test_to_graph_should_return_max_occurs_asterisk() -> None:
     g2 = Graph().parse(data=src, format="turtle")
 
     assert_isomorphic(g1, g2)
+
+
+def test_min_occurs_0(mocker: MockFixture) -> None:
+    """It returns a role graph isomorphic to spec."""
+    role = Role()
+    role.min_occurs = 0
+
+    src = """
+      @prefix dct: <http://purl.org/dc/terms/> .
+      @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+      @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+      @prefix dcat: <http://www.w3.org/ns/dcat#> .
+      @prefix modelldcatno: <https://data.norge.no/vocabulary/modelldcatno#> .
+      @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+      <http://example.com/.well-known/skolem/284db4d2-80c2-11eb-82c3-83e80baa2f94>
+          a modelldcatno:Role ;
+            xsd:minOccurs 0
+        .
+      """
+
+    skolemutils = SkolemUtils()
+
+    mocker.patch(
+        "skolemizer.Skolemizer.add_skolemization",
+        side_effect=skolemutils.get_skolemization,
+    )
+
+    g1 = Graph().parse(data=role.to_rdf(), format="turtle")
+    g2 = Graph().parse(data=src, format="turtle")
+
+    assert_isomorphic(g1, g2)
